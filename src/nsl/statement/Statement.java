f3aba40ac8d7c97a6af3c33770665ec58d0f186c
Jan T. Sott <git@idleberg.com>
2026-08-08 22:50:23 +0200

    feat: add IfShellVarContextAll instruction as ShellVarContextAll()

--- src/nsl/statement/Statement.java
+++ src/nsl/statement/Statement.java
@@ -218,6 +218,8 @@ public abstract class Statement {
 			return new IfRebootFlagInstruction(returns);
 		if (ScriptParser.tokenizer.match(IfRtlLanguageInstruction.name))
 			return new IfRtlLanguageInstruction(returns);
+		if (ScriptParser.tokenizer.match(IfShellVarContextAllInstruction.name))
+			return new IfShellVarContextAllInstruction(returns);
 		if (ScriptParser.tokenizer.match(IfSilentInstruction.name))
 			return new IfSilentInstruction(returns);
 		if (ScriptParser.tokenizer.match(InitPluginsDirInstruction.name))
